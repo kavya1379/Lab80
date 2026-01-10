ddf4dfe1885fbbf43b62df3e5a3c0ed2df194d76
kavya1379 <horkerikavya@gmail.com>
2026-01-10 13:24:54 +0530

    Update test_product.py

--- test_product.py
+++ test_product.py
@@ -1,11 +1,30 @@
-from product import product_info
+from projects.student import process_student_data
 
-def test_product_info():
-    expected_output = (
-        "Product ID: P102\n"
-        "Name: Keyboard\n"
-        "Quantity: 5\n"
-        "Price: 799.5"
-    )
-    result = product_info("P102", "Keyboard", 5, 799.5)
-    assert result ==expected_output
+def test_default_values():
+    result = process_student_data(["student.py"])
+
+    assert result["name"] == "Niyati Gogri"
+    assert result["department"] == "BCA"
+    assert result["semester"] == "3"
+    assert result["total"] == 283
+    assert result["grade"] == "A"
+
+def test_command_line_arguments():
+    args = [
+        "student.py",
+        "yashvi",
+        "BCA",
+        "3",
+        "70",
+        "75",
+        "80"
+    ]
+
+    result = process_student_data(args)
+
+    assert result["name"] == "yashvi"
+    assert result["total"] == 225
+    assert result["average"] == 75
+    assert result["grade"] == "C"
+
+
